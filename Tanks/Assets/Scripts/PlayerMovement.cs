f33114562fc2dd6dc7ab1f35c5858c8323ff8f55
programmerof23 <70703264+programmerof23@users.noreply.github.com>
2020-11-18 14:34:25 -0500

    Added Tank Poly
    Tank Polygon is now in the materials folder.

---
+++ Tanks/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle;
+    private Player Tank;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tank.transform.position = new Vector3(0.5f, 0f, 0f);
+        xAngle = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tank.transform.Rotate(xAngle, yAngle, zAngle);
+    }
+}
