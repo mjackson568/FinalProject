c4e4969ead3dc5211c9f75f04dc2d957279d70ba
Maurice Jackson <21JackM@live.elida.k12.oh.us>
2020-11-23 09:10:13 -0500

    tank movement kinda
    a movement system that kinda works

--- Tanks/Assets/Scripts/PlayerMovement.cs
+++ Tanks/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float xAngle, yAngle, zAngle;
-    public GameObject Tank;
-    // Start is called before the first frame update
+    public float movespeed = 3.0f;
+    public float rotationSpeed = 90.0f;
+    //public float xAngle, yAngle, zAngle;
+    //public GameObject Tank;
+     //Start is called before the first frame update
     void Start()
     {
-        Tank.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-        xAngle = 0.0f;
-        yAngle = 0.5f;
-        zAngle = 0.0f;
+        //Updated upstream
+        //Tank.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        //xAngle = 0.0f;
+        //yAngle = 0.5f;
+       // zAngle = 0.0f;
+
+       // Tank.transform.position = new Vector3(0.5f, 0f, 0f);
+        //xAngle = 0.5f;
+        //Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
-        Tank.transform.Rotate(xAngle, yAngle, zAngle);
+        float rotateTank = Input.GetAxis("Horizontal");
+        float moveTank = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody>().velocity = transform.forward * movespeed * moveTank;
+
+        transform.Rotate(Vector3.up * rotationSpeed * rotateTank * Time.deltaTime);
+        //Tank.transform.Rotate(xAngle, yAngle, zAngle);
     }
 }
